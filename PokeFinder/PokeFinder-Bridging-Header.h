cb5584ec61b9f0aec0564e986a5e1aa7e7e67376
Uplifted Studios <raphaelhidalgo3@gmail.com>
2017-07-27 14:55:10 -0700

    Added Geofire and implemented adding random pokemon feature

---
+++ PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Raphael M. Hidalgo on 7/27/17.
+//  Copyright © 2017 UpliftedStudios. All rights reserved.
+//
+
+#import "GeoFire.h"
